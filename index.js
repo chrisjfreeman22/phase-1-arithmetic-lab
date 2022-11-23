8d3692dbc18758fd3aca1c47167acf9616f77944
Chris Freeman <chrisjfreeman22@gmail.com>
2022-11-23 17:34:53 -0600

    commit tree 8cdc3f6949c97ce0c688392f1195a179a1ddeea4

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+let num1 = 31
+let num2 = 2
+let num3 = 24
+let num4 = 5
+let set = [1, 2, 20]
 
-// Write your code here
+function multiply(num1, num2){
+console.log( num1 * num2)
+}
+
+function random(){
+console.log( Math.floor(Math.random() * 10) + 1)
+}
+
+function mod(num3, num4){
+    console.log( num3 % num4)
+}
+
+function max(){
+console.log( Math.max(1, 2, 20))
+}
+max()
+mod(num3, num4)
+random()
+multiply(num1,num2)
